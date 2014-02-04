a2caaf9d141337b0ad11160103b506ce75d821ac
mnb-dade <mnb.dade@gmail.com>
2014-02-04 15:57:23 -0500

    Add bank account api example

---
+++ examples/bank_accounts/example.js
@@ -0,0 +1,90 @@
+$(function(){
+  
+  $('#submit_form').on('click', function(event){
+    event.preventDefault();
+    clearErrors();
+    
+    var form = $(this).closest('form');
+    var url = form.attr('action');
+    var results_list = $('#results');
+    
+    results_list.html('<li class="list-group-item">Searching..</li>');
+    
+    $.ajax({
+      dataType: "json",
+      url: url,
+      data: form.serialize()
+    })
+      .done(function( data ) {
+        renderResults(data);
+      })
+      .fail(function( jqxhr, textStatus, error ) {
+        renderError(jqxhr);
+    });
+    
+  });
+  
+  $('#clear_form').on('click', function(){
+    clearErrors();
+  });
+  
+});
+
+function renderResults(data){
+  var results_list = $('#results');
+
+  results_list.html('');
+  
+  if( data.length > 0 ){
+    $.each(data, function(i, result) {
+      var list_item = $('<li class="list-group-item"></li>');
+      var header = $('<h4 class="list-group-item-heading">Receivable account: '+ result.receivable_account_id +'</h4>');
+      var item_text = $('<p class="list-group-item-text"></p>');
+      
+      item_text.append('Bank Account ID: ' + result.bank_account.id + '<br>');
+      item_text.append('Account name: ' + result.bank_account.name + '<br>');
+      item_text.append('Routing number: ' + result.bank_account.routing_number + '<br>');
+      item_text.append('Account number: ' + result.bank_account.account_number);
+      
+      list_item.append(header);
+      list_item.append(item_text);
+      results_list.append(list_item);
+    });
+  }
+  else {
+    results_list.append('<li class="list-group-item">No results found</li>');
+  }
+}
+
+function renderError(jqxhr){
+  var results_list = $('#results');
+  results_list.html('');
+  
+  if( jqxhr.status == 401 ){
+    // Unauthorized
+    results_list.append('<li class="list-group-item error-message">Unauthorized Access</li>');
+  }
+  else if( jqxhr.status == 422 ){
+    
+    var error = $.parseJSON(jqxhr.responseText);
+    
+    if( error.code == "validation_failed" ){
+      results_list.append('<li class="list-group-item error-message">'+ error.message +'</li>');
+    
+      field_error_messages = $('<li class="list-group-item"></li>');
+    
+      $.each(error.errors, function(i, field_error) {
+        $("#" + field_error.field).closest('.form-group').addClass('has-error');
+        field_error_messages.append('<li class="field-error-message">'+ field_error.message +'</li>');
+      });
+    
+      results_list.append(field_error_messages);
+    }
+  }
+}
+
+function clearErrors(){
+  var results_list = $('#results');
+  results_list.html('');
+  $('.form-group').removeClass('has-error');
+}
